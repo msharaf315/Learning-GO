302379336baf336ee90ed0be11ca0eaad29f70ae
Mohammad-Sharafaldeen <msherf98@gmail.com>
2023-01-11 17:41:45 +0200

    init commit

---
+++ Models/product.go
@@ -0,0 +1,18 @@
+package main
+
+type Product struct {
+	id          string
+	title       string
+	description string
+	price       float64
+}
+
+func NewProduct(id string, title string, description string, price float64) *Product {
+	return &Product{
+		id,
+		title,
+		description,
+		price,
+	}
+
+}
